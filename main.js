6bcc0073f85b9d1c21190cbc4e4114ae26f53e80
joekennerly <joseph.kennerly@gmail.com>
2019-06-01 08:00:24 -0500

    Javascript Dom Manipulation Exercise

---
+++ main.js
@@ -0,0 +1,18 @@
+// Use JavaScript to obtain a reference to the first section with the class of article__header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
+let firstEl = document.querySelector(".article__header")
+firstEl.textContent = "Welcome to the Joe blog"
+
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
+let allEl = document.querySelectorAll(".article__header")
+// allEl[0].classList.add("important")
+// allEl[1].classList.add("important")
+for (let i = 0; i < allEl.length; i++) {
+  allEl[i].classList.add("important")
+}
+
+// Obtain a reference the element with a class of dashed and remove it.
+let dashed = document.querySelector(".dashed")
+dashed.classList.remove("dashed")
+
+// Obtain a reference the element with a class of article__footer and add the class of goldenrod it.
+document.querySelector(".article__footer").classList.add("goldenrod")
